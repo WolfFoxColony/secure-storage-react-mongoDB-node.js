d88e057ebe83ca1d45c8bc5c7013774c812108f9
ilumm <furt.kariina@gmail.com>
2025-08-09 10:38:00 +0300

    add upload file, get files, connect MongoDB, file schema

---
+++ client/src/components/UploadFile.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+const UploadFile = () => {
+    const [file, setFile] = useState<File | null>(null);
+    const [url, setUrl] = useState("");
+
+    const handleUpload = async () => {
+        if (!file) return;
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            const res = await axios.post("http://localhost:4000/upload", formData);
+            setUrl(res.data.url)
+        } catch (err) {
+            console.error("Upload error", err);
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)}/>
+            <button onClick={handleUpload}>Upload</button>
+            {url && (
+                <div>
+                    <p>Uploaded:</p>
+                    <img src={url} alt="Uploaded" width={200}/>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default UploadFile;
---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const cors = require("cors");
+const connectDB = require("./db");
+const File = require("./models/File");
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+}));
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const upload = multer({dest: "uploads/"});
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is working 🚀");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    try {
+        const result = await cloudinary.uploader.upload(req.file.path);
+
+        const fileRecord = new File({
+            originalName: req.file.originalname,
+            url: result.secure_url,
+        });
+        await fileRecord.save();
+
+        fs.unlinkSync(req.file.path);
+        res.json({url: result.secure_url});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Upload failed"});
+    }
+});
+
+app.get("/files", async (req, res) => {
+    try {
+        const files = await File.find().sort({uploadedAt: -1});
+        res.json(files);
+    } catch (err) {
+        res.status(500).json({message: "Failed to fetch files"});
+    }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
